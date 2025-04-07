354899595a397b218810e101ce526ec4d0e330a0
Martinmessner <martinmessner95@gmail.com>
2025-04-07 17:37:30 -0300

    fallback image for item not exist

--- src/app/helpers/ImageItemsChamps.tsx
+++ src/app/helpers/ImageItemsChamps.tsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import styles from "../page.module.css";
 
 const ImgItemsChampionsSummoners = ({ idItem }: { idItem: string }) => {
-  if (!idItem) {
-    return;
-  }
+  const [hasError, setHasError] = useState(false);
+
+  if (!idItem) return null;
+
   const itemChampion = `https://ddragon.leagueoflegends.com/cdn/13.15.1/img/item/${idItem}.png`;
+  const fallbackImage = "/fallback.png"; // Asegurate de tener esta imagen en tu carpeta `public`
 
   return (
     <img
-      className={`${styles["item-champion"]}`}
-      src={itemChampion}
+      className={styles["item-champion"]}
+      src={hasError ? fallbackImage : itemChampion}
       alt="item"
       title={idItem}
+      onError={() => setHasError(true)}
     />
   );
 };
